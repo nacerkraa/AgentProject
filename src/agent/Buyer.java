99cc3763a7c48999f137eddfda963b4a48e44018
nacer kraa <79715038+nacerkraa@users.noreply.github.com>
2023-01-05 00:07:09 +0100

    Add TickerBehaviour method
    TickerBehaviour it's an method that run everry beriod of time.

--- src/agent/Buyer.java
+++ src/agent/Buyer.java
@@ -2,6 +2,7 @@ package agent;
 
 import jade.core.Agent;
 import jade.core.Location;
+import jade.core.behaviours.TickerBehaviour;
 
 /**
  * Create agent Acheteur exteds from Agent
@@ -18,7 +19,17 @@ public class Buyer extends Agent {
 		Object[] args = getArguments();
 		if(args.length == 1) {
 			book = (String) args[0];
-			System.out.println("Buyer Agent: " + this.getAID() + " I'm searching to buy the book: " + book);
+			System.out.println("Buyer Agent: " + this.getAID() 
+			+ " I'm searching to buy the book: " + book);
+			addBehaviour(new TickerBehaviour(this, 1000) {
+				private int con = 0;
+				@Override
+				protected void onTick() {
+					++con;
+					System.out.println("attempt to buy number: " + con);
+					
+				}
+			});
 		} else {
 			System.out.println("You should be enter one book");
 			doDelete();
